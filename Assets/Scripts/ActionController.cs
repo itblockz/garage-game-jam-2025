067bf50fca8e2d8404935526ffdf69c46b387f8a
ItBlockZ <nontapat.boss@gmail.com>
2025-07-12 18:40:50 +0700

    Add select feedback

--- Assets/Scripts/ActionController.cs
+++ Assets/Scripts/ActionController.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ActionController : MonoBehaviour
 {
     private Action action;
+    private Image actionImage;
 
     public void PerformAction()
     {
@@ -30,5 +32,15 @@ public class ActionController : MonoBehaviour
         }
         action = newAction;
         Debug.Log("Action set to: " + action.GetType().Name);
+    }
+
+    public void SetActionImage(Image newImage)
+    {
+        if (actionImage != null)
+        {
+            actionImage.color = Color.white; // Reset previous action image color
+        }
+        newImage.color = Color.gray;
+        actionImage = newImage;
     }
 }
